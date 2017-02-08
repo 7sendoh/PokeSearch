e32ba40014deaf653a80a7fb4f5bc8eec7affbca
7sendoh <ringo@Pings-Mac-mini.local>
2017-02-08 16:59:45 -0500

    Initial Commit

---
+++ PokeSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeSearch
+//
+//  Created by Ping Li on 2017-02-08.
+//  Copyright © 2017 Ping Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
